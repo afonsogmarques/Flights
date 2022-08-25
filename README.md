6bde4997866b3d4658a41c88b1ddae0eb4450d62
afonsogmarques <afonsogmarques@gmail.com>
2022-08-25 17:49:50 +0100

    added front end search functionality in homepage

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Flgith Search Engine
+# Reverse Flight Search Engine
 
-Final Poject for CS50.
+A flight search engine that ultimately should allow the user to browse all the scheduled flights to a provided destination on a provided date and their price, not from a selected departure location but from all of the possible ones, sorted from cheapest to most expensive.
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 import json
-
+import sqlite3
+from aiosqlite import connect
 from flask import Flask, redirect, render_template, request
 from amadeus import Client, ResponseError
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
@@ -26,9 +27,29 @@ amadeus = Client(
 # except ResponseError as error:
 #     print(error)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    if request.method == 'POST':
+        destination = request.form.get('destination')
+        date = request.form.get('date')
+        return render_template('results.html', date=date, destination=destination)
+        
+    else:
+        with sqlite3.connect('airports.db', check_same_thread=False) as con:
+            cursor = con.cursor()
+            cursor.execute('SELECT name FROM airports ORDER BY name')
+            rows = cursor.fetchall()
+
+            airport_names = []
+            for row in rows:
+                row = row[0]
+                airport_names.append(row)
+                
+            for name in airport_names:
+                if None in airport_names:
+                    airport_names.remove(None)
+            return render_template('index.html', airport_names=airport_names)
 
 @app.route('/search')
 def search():
